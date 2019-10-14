c497fac455713e03930601784cac9d8580b2bd30
Federico <fede.pazos95@gmail.com>
2019-10-14 14:48:39 -0300

    Adds loaders to app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const config = require("./config");
+const loaders = require("./loaders");
 
 const app = express();
+
+(async function (app) {
+  await loaders.init(app);
+  return app;
+})(app);
 
 module.exports = app.listen(config.port);
 
